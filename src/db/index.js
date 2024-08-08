e8b2cee5f37cebf3115803c7f346f9085a268710
deepu-paswan-1123 <159120399+deepu-paswan-1123@users.noreply.github.com>
2024-08-08 12:51:24 +0530

    node demo

---
+++ src/db/index.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+import { DB_NAME } from '../constant.js';
+
+const connectDB = async () => {
+  try {
+    console.log('MONGODB_URI:', process.env.MONGODB_URI); // Log the URI to verify it
+    const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`\nMONGODB connected !! DB HOST: ${connectionInstance.connection.host}`);
+  } catch (error) {
+    console.log('MONGODB connection error ', error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
